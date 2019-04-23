9f18b77c8b341d02ce0d459bbe4a4b2b9652d3f2
Oscar Ostlund <oscar.ostlund@gmail.com>
2019-04-22 23:10:53 -0400

    Print in dec

--- src/SPI.cpp
+++ src/SPI.cpp
@@ -7,7 +7,7 @@ void SPIObject::beginTransaction(const SPISettings &s) { _address = 0; }
 byte SPIObject::transfer(byte val) {
 
   if (_address == 0) {
-    std::cout << "Set Address = " << std::hex << static_cast<int>(val) << "\n";
+    std::cout << "Set Address = " << static_cast<int>(val) << "\n";
 
     _address = val;
     return 0;
@@ -15,11 +15,11 @@ byte SPIObject::transfer(byte val) {
 
   byte data[2]{_address & 0x7E, val};
   wiringPiSPIDataRW(0, &data[0], 2);
-  std::cout << "Address = " << std::hex << static_cast<int>(_address)
-            << " Data[0] = " << std::hex << static_cast<int>(data[0])
-            << " Data[1] = " << std::hex << static_cast<int>(data[1])
-            << " Wrote = " << std::hex << static_cast<int>(val)
-            << " Read = " << std::hex << static_cast<int>(data[1]) << "\n";
+  std::cout << "Address = " << static_cast<int>(_address)
+            << " Data[0] = " << static_cast<int>(data[0])
+            << " Data[1] = " << static_cast<int>(data[1])
+            << " Wrote = " << static_cast<int>(val)
+            << " Read = " << static_cast<int>(data[1]) << "\n";
 
   return data[1];
 }
