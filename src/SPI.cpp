1356e1c17671fc6eb180354004df29059dff62ca
Oscar Ostlund <oscar.ostlund@gmail.com>
2019-04-22 23:04:35 -0400

    Print data

--- src/SPI.cpp
+++ src/SPI.cpp
@@ -14,6 +14,8 @@ byte SPIObject::transfer(byte val) {
   byte data[2]{_address & 0x7E, val};
   wiringPiSPIDataRW(0, &data[0], 2);
   std::cout << "Address = " << std::hex << static_cast<int>(_address)
+            << " Data[0] = " << std::hex << static_cast<int>(data[0])
+            << " Data[1] = " << std::hex << static_cast<int>(data[1])
             << " Wrote = " << std::hex << static_cast<int>(val)
             << " Read = " << std::hex << static_cast<int>(data[1]) << "\n";
 
