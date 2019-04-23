6dd6e1906d6509d78ffc9968e7be3fc0416b1cf3
Oscar Ostlund <oscar.ostlund@gmail.com>
2019-04-22 22:00:17 -0400

    REfactor SPI. Fix bug

--- src/SPI.cpp
+++ src/SPI.cpp
@@ -1,3 +1,31 @@
 #include "SPI.h"
 
 SPIObject SPI;
+
+void SPIObject::beginTransaction(const SPISettings &s) {
+  _address = 0;
+  // std::cout << "Resetting address to 0 \n";
+}
+
+byte SPIObject::transfer(byte val) {
+  byte data[2];
+
+  if (_address == 0) {
+    _address = val;
+    data[0] = val & 0x7E;
+    std::cout << "Setting address to " << std::hex << static_cast<int>(data[0])
+              << "\n";
+    return 0;
+  }
+  data[1] = val;
+  wiringPiSPIDataRW(0, &data[0], 2);
+  std::cout << "Wrote " << std::hex << static_cast<int>(val) << " and read "
+            << std::hex << static_cast<int>(data[1]) << "\n";
+
+  return data[1];
+}
+
+void SPIObject::endTransaction() {
+  _address = 0;
+  // std::cout << "Resetting address to 0 \n";
+}
--- src/SPI.h
+++ src/SPI.h
@@ -15,33 +15,9 @@ struct SPISettings {
 
 class SPIObject {
 public:
-  inline void beginTransaction(const SPISettings &s) {
-    _address = 0;
-    // std::cout << "Resetting address to 0 \n";
-  }
-
-  inline byte transfer(byte val) {
-    byte data[2];
-
-    if (_address == 0) {
-      _address = val;
-      data[0] = (val << 1) & 0x7E;
-      std::cout << "Setting address to " << std::hex
-                << static_cast<int>(data[0]) << "\n";
-      return 0;
-    }
-    data[1] = val;
-    wiringPiSPIDataRW(0, &data[0], 2);
-    std::cout << "Wrote " << std::hex << static_cast<int>(val) << " and read "
-              << std::hex << static_cast<int>(data[1]) << "\n";
-
-    return data[1];
-  }
-
-  inline void endTransaction() {
-    _address = 0;
-    // std::cout << "Resetting address to 0 \n";
-  }
+  void beginTransaction(const SPISettings &s);
+  byte transfer(byte val);
+  void endTransaction();
 
   SPIObject() : _address{0} {
 
